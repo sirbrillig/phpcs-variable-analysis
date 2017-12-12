7eb1e38dfa08e55a9c75e4a20b7dcb98fe64852e
Payton Swick <payton@foolord.com>
2017-12-12 18:29:45 -0500

    Fix style of ScopeInfo, VariableInfo

--- VariableAnalysis/ScopeInfo.php
+++ VariableAnalysis/ScopeInfo.php
@@ -6,13 +6,13 @@ namespace VariableAnalysis;
  * Holds details of a scope.
  */
 class ScopeInfo {
-    public $owner;
-    public $opener;
-    public $closer;
-    public $variables = array();
+  public $owner;
+  public $opener;
+  public $closer;
+  public $variables = array();
 
-    function __construct($currScope) {
-        $this->owner = $currScope;
-        // TODO: extract opener/closer
-    }
+  public function __construct($currScope) {
+    $this->owner = $currScope;
+    // TODO: extract opener/closer
+  }
 }
--- VariableAnalysis/VariableInfo.php
+++ VariableAnalysis/VariableInfo.php
@@ -6,27 +6,27 @@ namespace VariableAnalysis;
  * Holds details of a variable within a scope.
  */
 class VariableInfo {
-    public $name;
-    /**
-     * What scope the variable has: local, param, static, global, bound
-     */
-    public $scopeType;
-    public $typeHint;
-    public $passByReference = false;
-    public $firstDeclared;
-    public $firstInitialized;
-    public $firstRead;
-    public $ignoreUnused = false;
+  public $name;
+  /**
+   * What scope the variable has: local, param, static, global, bound
+   */
+  public $scopeType;
+  public $typeHint;
+  public $passByReference = false;
+  public $firstDeclared;
+  public $firstInitialized;
+  public $firstRead;
+  public $ignoreUnused = false;
 
-    static $scopeTypeDescriptions = array(
-        'local'  => 'variable',
-        'param'  => 'function parameter',
-        'static' => 'static variable',
-        'global' => 'global variable',
-        'bound'  => 'bound variable',
-    );
+  public static $scopeTypeDescriptions = array(
+    'local'  => 'variable',
+    'param'  => 'function parameter',
+    'static' => 'static variable',
+    'global' => 'global variable',
+    'bound'  => 'bound variable',
+  );
 
-    function __construct($varName) {
-        $this->name = $varName;
-    }
+  public function __construct($varName) {
+    $this->name = $varName;
+  }
 }
