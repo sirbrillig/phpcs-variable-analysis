2b5cd4418239a41281dd52cb1b8d09911bd0a1c1
Payton Swick <payton@foolord.com>
2018-09-08 16:24:56 -0400

    Remove return types
    Return type declarations were added in PHP 7

--- VariableAnalysis/Tests/BaseTestCase.php
+++ VariableAnalysis/Tests/BaseTestCase.php
@@ -7,7 +7,7 @@ use PHP_CodeSniffer\Ruleset;
 use PHP_CodeSniffer\Config;
 
 class BaseTestCase extends TestCase {
-  public function prepareLocalFileForSniffs($sniffFiles, $fixtureFile): LocalFile {
+  public function prepareLocalFileForSniffs($sniffFiles, $fixtureFile) {
     $config = new Config();
     $ruleset = new Ruleset($config);
     if (! is_array($sniffFiles)) {
@@ -21,21 +21,21 @@ class BaseTestCase extends TestCase {
     return new LocalFile($fixtureFile, $ruleset, $config);
   }
 
-  public function getLineNumbersFromMessages(array $messages): array {
+  public function getLineNumbersFromMessages(array $messages) {
     $lines = array_keys($messages);
     sort($lines);
     return $lines;
   }
 
-  public function getWarningLineNumbersFromFile(LocalFile $phpcsFile): array {
+  public function getWarningLineNumbersFromFile(LocalFile $phpcsFile) {
     return $this->getLineNumbersFromMessages($phpcsFile->getWarnings());
   }
 
-  public function getErrorLineNumbersFromFile(LocalFile $phpcsFile): array {
+  public function getErrorLineNumbersFromFile(LocalFile $phpcsFile) {
     return $this->getLineNumbersFromMessages($phpcsFile->getErrors());
   }
 
-  public function getSniffFiles(): array {
+  public function getSniffFiles() {
     return [__DIR__ . '/../../VariableAnalysis/Sniffs/CodeAnalysis/VariableAnalysisSniff.php'];
   }
 
