90fabb94b0eb90a46cb808f6c59b796f4a3dc1f3
Payton Swick <payton@foolord.com>
2019-03-25 19:20:08 -0400

    Tests: Add naked static, var to class properties

--- VariableAnalysis/Tests/CodeAnalysis/fixtures/AnonymousClassWithPropertiesFixture.php
+++ VariableAnalysis/Tests/CodeAnalysis/fixtures/AnonymousClassWithPropertiesFixture.php
@@ -4,9 +4,11 @@ class ClassWithAnonymousClass {
     public function getAnonymousClass() {
         return new class {
             protected $storedHello;
-            private static $storedHello;
-            private $storedHello;
+            private static $storedHello2;
+            private $storedHello3;
             public $helloOptions = [];
+            static $aStaticOne;
+            var $aVarOne;
             public function sayHelloWorld() {
                 echo "hello world";
             }
