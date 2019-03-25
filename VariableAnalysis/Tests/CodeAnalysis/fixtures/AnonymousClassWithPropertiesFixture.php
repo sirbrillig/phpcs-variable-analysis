a60affa52200870c52750abc8a940157f0cf3c64
Payton Swick <payton@foolord.com>
2019-03-25 19:13:59 -0400

    Tests: add 'private static' to anon class fixture

--- VariableAnalysis/Tests/CodeAnalysis/fixtures/AnonymousClassWithPropertiesFixture.php
+++ VariableAnalysis/Tests/CodeAnalysis/fixtures/AnonymousClassWithPropertiesFixture.php
@@ -4,6 +4,8 @@ class ClassWithAnonymousClass {
     public function getAnonymousClass() {
         return new class {
             protected $storedHello;
+            private static $storedHello;
+            private $storedHello;
             public $helloOptions = [];
             public function sayHelloWorld() {
                 echo "hello world";
