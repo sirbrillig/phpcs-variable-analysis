244ddad9530a7e40655d0f24fbae55e4dd2c04e8
Payton Swick <payton@foolord.com>
2019-04-01 00:13:18 -0400

    Tests: Try to add test for #79

--- VariableAnalysis/Tests/CodeAnalysis/fixtures/ClassWithMembersFixture.php
+++ VariableAnalysis/Tests/CodeAnalysis/fixtures/ClassWithMembersFixture.php
@@ -2,21 +2,21 @@
 
 class ClassWithoutMembers {
     function method_without_param() {
-        echo $var;
-        echo "xxx $var xxx";
-        echo "xxx {$var} xxx";
-        echo "xxx $var $var2 xxx";
-        echo "xxx {$var} {$var2} xxx";
-        func($var);
-        func(12, $var);
-        func($var, 12);
-        func(12, $var, 12);
+        echo $var; // this should be a warning
+        echo "xxx $var xxx"; // this should be a warning
+        echo "xxx {$var} xxx"; // this should be a warning
+        echo "xxx $var $var2 xxx"; // this should be a warning
+        echo "xxx {$var} {$var2} xxx"; // this should be a warning
+        func($var); // this should be a warning
+        func(12, $var); // this should be a warning
+        func($var, 12); // this should be a warning
+        func(12, $var, 12); // this should be a warning
         $var = 'set the var';
         echo $var;
         echo "xxx $var xxx";
         echo "xxx {$var} xxx";
-        echo "xxx $var $var2 xxx";
-        echo "xxx {$var} {$var2} xxx";
+        echo "xxx $var $var2 xxx"; // this should be a warning
+        echo "xxx {$var} {$var2} xxx"; // this should be a warning
         func($var);
         func(12, $var);
         func($var, 12);
@@ -63,7 +63,7 @@ class ClassWithLateStaticBinding {
 
     static function method_with_late_static_binding($param) {
         static::some_method($param);
-        static::some_method($var); // should report a warning
+        static::some_method($var); // should report a warning // this should be a warning
         static::some_method(static::CONSTANT, $param);
         $called_class = get_called_class();
         echo $called_class::$static_member_var;
@@ -74,5 +74,27 @@ class ChildClassWithMembers extends ClassWithMembers {
     function method_with_parent_reference() {
         echo self::$static_member_var;
         echo parent::$no_such_static_member_var;
+    }
+}
+
+class ClassWithAssignedMembers {
+    public $member_var = 'hello';
+    private $private_member_var = 'hi';
+    protected $protected_member_var = 'foo';
+    static $static_member_var = 'bar';
+
+    function method_with_member_var() {
+        echo $this->member_var;
+        echo $this->no_such_member_var;
+        echo self::$static_member_var;
+        echo self::$no_such_static_member_var;
+        echo SomeOtherClass::$external_static_member_var;
+    }
+
+    function method_with_static_assigned_member_var() {
+        static $new_var = null;
+        if ($new_var === null) {
+            echo 'it is null';
+        }
     }
 }
