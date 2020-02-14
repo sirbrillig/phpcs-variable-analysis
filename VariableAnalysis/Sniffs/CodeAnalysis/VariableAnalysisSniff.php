d62a1c984351ff931ea59c510afb4c293fde63d8
jrfnl <jrfnl@users.noreply.github.com>
2020-02-14 02:54:33 +0100

    VariableAnalysisSniff::checkForStaticMember(): fix comment tolerance [1]
    PHP ignores comments in unexpected/unconventional places and so should the sniff.
    
    Includes adjusting an existing unit test.
    
    Without the fix, the adjusted unit test would cause test failures.

--- VariableAnalysis/Sniffs/CodeAnalysis/VariableAnalysisSniff.php
+++ VariableAnalysis/Sniffs/CodeAnalysis/VariableAnalysisSniff.php
@@ -8,6 +8,7 @@ use VariableAnalysis\Lib\Constants;
 use VariableAnalysis\Lib\Helpers;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
 
 class VariableAnalysisSniff implements Sniff {
   /**
@@ -667,11 +668,11 @@ class VariableAnalysisSniff implements Sniff {
   protected function checkForStaticMember(File $phpcsFile, $stackPtr, $varName, $currScope) {
     $tokens = $phpcsFile->getTokens();
 
-    $doubleColonPtr = $stackPtr - 1;
-    if ($tokens[$doubleColonPtr]['code'] !== T_DOUBLE_COLON) {
+    $doubleColonPtr = $phpcsFile->findPrevious(Tokens::$emptyTokens, $stackPtr - 1, null, true);
+    if ($doubleColonPtr === false || $tokens[$doubleColonPtr]['code'] !== T_DOUBLE_COLON) {
       return false;
     }
-    $classNamePtr = $stackPtr - 2;
+    $classNamePtr = $phpcsFile->findPrevious(Tokens::$emptyTokens, $doubleColonPtr - 1, null, true);
     $staticReferences = [
       T_STRING,
       T_SELF,
@@ -679,7 +680,7 @@ class VariableAnalysisSniff implements Sniff {
       T_STATIC,
       T_VARIABLE,
     ];
-    if (! in_array($tokens[$classNamePtr]['code'], $staticReferences, true)) {
+    if ($classNamePtr === false || ! in_array($tokens[$classNamePtr]['code'], $staticReferences, true)) {
       return false;
     }
     // "When calling static methods, the function call is stronger than the
