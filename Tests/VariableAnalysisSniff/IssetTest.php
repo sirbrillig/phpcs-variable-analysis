5bddc931c3b6f3eda5b7cb83dc73dd41f60f2b1a
Payton Swick <payton@foolord.com>
2020-09-07 16:54:42 -0400

    Add tests for isset and empty

---
+++ Tests/VariableAnalysisSniff/IssetTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace VariableAnalysis\Tests\VariableAnalysisSniff;
+
+use VariableAnalysis\Tests\BaseTestCase;
+
+class IssetTest extends BaseTestCase {
+  public function testIssetVariableUse() {
+    $fixtureFile = $this->getFixture('IssetFixture.php');
+    $phpcsFile = $this->prepareLocalFileForSniffs($fixtureFile);
+    $phpcsFile->process();
+    $lines = $this->getWarningLineNumbersFromFile($phpcsFile);
+    $expectedWarnings = [
+      4,
+      23, // ideally this should not be a warning, but will be because it is difficult to know: https://github.com/sirbrillig/phpcs-variable-analysis/issues/202#issuecomment-688507314
+    ];
+    $this->assertEquals($expectedWarnings, $lines);
+  }
+}
