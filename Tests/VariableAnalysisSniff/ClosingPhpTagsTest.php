72cb4f923d15a8dd4e4bc7230c7b72ebc43a67b1
Payton Swick <payton@foolord.com>
2021-06-18 15:49:58 -0400

    Allow parsing mixed HTML and PHP (#234)
    * Add test for mixed html and php
    
    * Consider html and closing tags to be empty tokens
    
    * Remove explicit return type to support PHP 5.6
    
    * Add additional test for sniff codes for closing tags fixture

---
+++ Tests/VariableAnalysisSniff/ClosingPhpTagsTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace VariableAnalysis\Tests\VariableAnalysisSniff;
+
+use VariableAnalysis\Tests\BaseTestCase;
+
+class ClosingPhpTagsTest extends BaseTestCase {
+  public function testVariableWarningsWhenClosingTagsAreUsed() {
+    $fixtureFile = $this->getFixture('ClosingPhpTagsFixture.php');
+    $phpcsFile = $this->prepareLocalFileForSniffs($fixtureFile);
+    $phpcsFile->process();
+    $lines = $this->getWarningLineNumbersFromFile($phpcsFile);
+    $expectedWarnings = [
+      6,
+      8,
+      13,
+      16,
+    ];
+    $this->assertEquals($expectedWarnings, $lines);
+  }
+
+  public function testVariableWarningsHaveCorrectSniffCodesWhenClosingTagsAreUsed() {
+    $fixtureFile = $this->getFixture('ClosingPhpTagsFixture.php');
+    $phpcsFile = $this->prepareLocalFileForSniffs($fixtureFile);
+    $phpcsFile->process();
+    $warnings = $phpcsFile->getWarnings();
+    $this->assertEquals('VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable', $warnings[6][1][0]['source']);
+    $this->assertEquals('VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable', $warnings[8][6][0]['source']);
+    $this->assertEquals('VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable', $warnings[13][1][0]['source']);
+    $this->assertEquals('VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable', $warnings[16][6][0]['source']);
+  }
+}
