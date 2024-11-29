b63cb8a5906d96683eb2fa3dd3b65328e1b5ff84
Payton Swick <payton@foolord.com>
2024-11-29 13:32:54 -0500

    Add test for anon class with typehint properties

--- Tests/VariableAnalysisSniff/fixtures/AnonymousClassWithPropertiesFixture.php
+++ Tests/VariableAnalysisSniff/fixtures/AnonymousClassWithPropertiesFixture.php
@@ -41,3 +41,28 @@ $anonClass = new class() { // should trigger unused warning
         echo static::$storedHello;
     }
 };
+
+class ClassWithAnonymousClassAndTypeHints
+{
+		int $main_id = 1;
+		public int $id = 1;
+		public \My\Data|bool $data;
+
+		public function test_1(): object
+		{
+				return new class
+				{
+						int $main_id = 1;
+						public int $id = 123456;
+						public \My\Data|bool $data;
+				};
+		}
+
+		public function test_2(): object
+		{
+				return new class
+				{
+						public $id = 123456;
+				};
+		}
+}
