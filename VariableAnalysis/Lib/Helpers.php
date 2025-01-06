fe417086559b621977b409f1d3097c25837e5830
Payton Swick <payton@foolord.com>
2025-01-06 10:33:03 -0500

    Still look for arrow func when var scope is 0
    `findVariableScopeExceptArrowFunctions()` can return null if it finds no
    scope, but it can also return 0 which is the file level scope. The
    additional code to look for arrow function scope needs to operate even
    on 0.

--- VariableAnalysis/Lib/Helpers.php
+++ VariableAnalysis/Lib/Helpers.php
@@ -429,7 +429,8 @@ class Helpers
 		$varName = isset($varName) ? $varName : self::normalizeVarName($token['content']);
 
 		$enclosingScopeIndex = self::findVariableScopeExceptArrowFunctions($phpcsFile, $stackPtr);
-		if ($enclosingScopeIndex) {
+
+		if (!is_null($enclosingScopeIndex)) {
 			$arrowFunctionIndex = self::getContainingArrowFunctionIndex($phpcsFile, $stackPtr, $enclosingScopeIndex);
 			$isTokenInsideArrowFunctionBody = is_int($arrowFunctionIndex);
 			if ($isTokenInsideArrowFunctionBody) {
